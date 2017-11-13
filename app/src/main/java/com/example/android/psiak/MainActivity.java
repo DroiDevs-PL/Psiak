8e00457d78269b2d2425eb776ec844938e694819
Paweł Zarzycki <pawel.zarzycki1990@gmail.com>
2017-11-13 11:09:48 +0100

    ADD adding menu

--- app/src/main/java/com/example/android/psiak/MainActivity.java
+++ app/src/main/java/com/example/android/psiak/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.android.psiak;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.settings) {
+            Toast.makeText(this, "Tutaj będą ustawienia!", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu=menu;
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem searchModeMenuItem = menu.findItem(R.id.settings);
+        return true;
     }
 }

21d910cfab2d99aea3660759fb2f27eefb3b17b7
pawel <pawel.zarzycki1990@gmail.com>
2017-11-13 21:52:27 +0100

    ADD nawigation drawer

--- app/src/main/java/com/example/android/psiak/MainActivity.java
+++ app/src/main/java/com/example/android/psiak/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private Menu menu;
+//adding nawigation drawer
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

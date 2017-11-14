3ba9fa242048293958d097490a2f53c5433bf80e
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2017-11-14 13:47:07 +0100

    UPDATE Dog Firebase Model

--- app/src/main/java/com/example/android/psiak/Model/TestDogFirebase.java
+++ app/src/main/java/com/example/android/psiak/Model/TestDogFirebase.java
@@ -5,15 +5,32 @@ package com.example.android.psiak.Model;
  */
 
 public class TestDogFirebase {
-    public String username;
-    public String locationCity;
+
+    // region Public Properties
+
+    public String dogName;
+    public String dogLocationCity;
+    public String category;
+    public String description;
+
+    // endregion
+
+    // region Initializers
 
     public TestDogFirebase() {
         // Default constructor required for calls to DataSnapshot.getValue(TestDogFirebase.class)
     }
 
-    public TestDogFirebase(String username, String email) {
-        this.username = username;
-        this.locationCity = email;
+    public TestDogFirebase(String dogName, String dogLocationCity) {
+        this.dogName = dogName;
+        this.dogLocationCity = dogLocationCity;
     }
+
+    public TestDogFirebase(String dogName, String category, String description) {
+        this.dogName = dogName;
+        this.category = category;
+        this.description = description;
+    }
+
+    // endregion
 }

25fe449efa26525d496fa88d7943f410434fae70
pawel <pawel.zarzycki1990@gmail.com>
2017-11-14 14:48:16 +0100

    ADD Adding ui elements declaration with Butterknife

--- app/src/main/java/com/example/android/psiak/MainActivity.java
+++ app/src/main/java/com/example/android/psiak/MainActivity.java
@@ -4,19 +4,37 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.doggie)
+    ImageView doggie;
+    @BindView(R.id.noDogs)
+    TextView noDogs;
+    @BindView(R.id.woof)
+    TextView woof;
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.activity_main)
+    RelativeLayout activityMain;
     private Menu menu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
     }
 
+    //region menu methods
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
@@ -33,4 +51,5 @@ public class MainActivity extends AppCompatActivity {
         MenuItem searchModeMenuItem = menu.findItem(R.id.settings);
         return true;
     }
+    //endregion
 }

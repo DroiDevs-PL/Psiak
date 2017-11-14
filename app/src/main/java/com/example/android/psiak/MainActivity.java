92c8ea7ace84f00121e2ddc0ceed9d57f1ce9c16
pawel <pawel.zarzycki1990@gmail.com>
2017-11-14 18:33:05 +0100

    ADD Adding navigationBar

--- app/src/main/java/com/example/android/psiak/MainActivity.java
+++ app/src/main/java/com/example/android/psiak/MainActivity.java
@@ -4,7 +4,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,16 +28,63 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     @BindView(R.id.activity_main)
     RelativeLayout activityMain;
+    @BindView(R.id.navList)
+    ListView navList;
+
     private Menu menu;
-//adding nawigation drawer
+
+    //adding nawigation drawer
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        setUpNavigationDrawer();
+
 
     }
+
+    //region navigationDrawer
+    private void setUpNavigationDrawer() {
+        addDrawerItems();
+        navList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //toDO refactor this to use id instead of position in case order changes
+                switch (position) {
+                    case 0: {
+                        Toast.makeText(MainActivity.this, "Główna", Toast.LENGTH_SHORT).show();
+                    }
+                    case 1: {
+                        Toast.makeText(MainActivity.this, "Fav", Toast.LENGTH_SHORT).show();
+                    }
+                    case 2: {
+                        Toast.makeText(MainActivity.this, "Prefs", Toast.LENGTH_SHORT).show();
+                    }
+                    case 3: {
+                        Toast.makeText(MainActivity.this, "AvailableShelters", Toast.LENGTH_SHORT).show();
+                    }
+                    case 4: {
+                        Toast.makeText(MainActivity.this, "Procedure", Toast.LENGTH_SHORT).show();
+                    }
+                    case 5: {
+                        Toast.makeText(MainActivity.this, "About to do", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+
+    private void addDrawerItems() {
+        String[] osArray = {"Główna", "Ulubione", "Preferencje", "Dostępne schroniska", "Procedura adopcyjna", "O nas"};
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        navList.setAdapter(mAdapter);
+    }
+    //endregion
+
 
     //region menu methods
     @Override

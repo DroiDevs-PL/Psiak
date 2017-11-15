86d84d8c26b351f7521062c16c8fcfe2bfcf70cf
radek <kedar03@gmail.com>
2017-11-15 03:24:53 +0100

    Add Retrofit

--- app/src/main/java/com/example/android/psiak/MainActivity.java
+++ app/src/main/java/com/example/android/psiak/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.android.psiak;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -9,10 +10,21 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.android.psiak.Model.Dog;
+import com.example.android.psiak.Network.DummyDogDataService;
+
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final String TAG = MainActivity.class.getName();
 
     @BindView(R.id.doggie)
     ImageView doggie;
@@ -31,6 +43,33 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        Retrofit retrofit = ((DoggieApplication)getApplication()).getRetrofitInstance();
+        DummyDogDataService dummyDogDataService = retrofit.create(DummyDogDataService.class);
+        Call<List<Dog>> dogCall = dummyDogDataService.loadDog();
+        dogCall.enqueue(new Callback<List<Dog>>() {
+            @Override
+            public void onResponse(Call<List<Dog>> call, Response<List<Dog>> response) {
+                if(response.isSuccessful()) {
+                    List<Dog> dogs = response.body();
+                   // String dogsList = dogs.stream().reduce("", (d1, d2) -> d1.toString() + d2.toString()));
+                    String dogsListString = "";
+                    for(Dog d : dogs) {
+                        dogsListString = dogsListString + d + "\n";
+                    }
+                    textView.setText(dogsListString);
+
+                }
+                else {
+                    int httpCode = response.code();
+                    textView.setText(Integer.toString(httpCode));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Dog>> call, Throwable t) {
+                Log.e(TAG, t.getMessage());
+            }
+        });
 
     }
 

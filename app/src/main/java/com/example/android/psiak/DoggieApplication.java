d0b41f3c1f68cc90682bf22f566887d335151116
radek <kedar03@gmail.com>
2017-11-15 01:11:13 +0100

    Add custom Application class

---
+++ app/src/main/java/com/example/android/psiak/DoggieApplication.java
@@ -0,0 +1,15 @@
+package com.example.android.psiak;
+
+import android.app.Application;
+
+/**
+ * Created by Radek on 15.11.2017.
+ */
+
+public class DoggieApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

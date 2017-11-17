f2b5d82bc85f685bac5b3949ee35cb0125134615
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2017-11-17 11:17:01 +0100

    UPDATE Example For Using Firebase

--- app/src/main/java/com/example/android/psiak/FirebaseTesting.java
+++ app/src/main/java/com/example/android/psiak/FirebaseTesting.java
@@ -3,18 +3,23 @@ package com.example.android.psiak;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.android.psiak.Firebase.FirebaseHelper;
+import com.example.android.psiak.Firebase.FirebaseDataListener;
+import com.example.android.psiak.Model.TestDogFirebase;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class FirebaseTesting extends AppCompatActivity {
+public class FirebaseTesting extends AppCompatActivity implements FirebaseDataListener {
 
     private static final String TAG = FirebaseTesting.class.toString();
 
@@ -46,7 +51,7 @@ public class FirebaseTesting extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        firebaseHelper = new FirebaseHelper();
+        firebaseHelper = new FirebaseHelper(this);
 
         firebaseHelper.getAllDogs();
 
@@ -79,6 +84,11 @@ public class FirebaseTesting extends AppCompatActivity {
         firebaseHelper.writeNewDog(dogName, dogCategory, dogDescription);
 
         Toast.makeText(getBaseContext(), "New dog added", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void setDogsData(ArrayList<TestDogFirebase> dogsData) {
+        Log.e(TAG, "Dogs count " + " " + dogsData.size());
     }
 
     // endregion

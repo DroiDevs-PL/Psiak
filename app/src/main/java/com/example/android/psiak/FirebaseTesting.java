aafec746d6fb18bf049616c0790b680b0714daa8
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2017-11-17 19:57:04 +0100

    UPDATE Bind Views To Testing Activity

--- app/src/main/java/com/example/android/psiak/FirebaseTesting.java
+++ app/src/main/java/com/example/android/psiak/FirebaseTesting.java
@@ -34,10 +34,32 @@ public class FirebaseTesting extends AppCompatActivity implements FirebaseDataLi
 
     @BindView(R.id.et_dog_name)
     EditText etDogName;
-    @BindView(R.id.et_dog_category)
-    EditText etDogCateory;
+    @BindView(R.id.et_dog_gender)
+    EditText etDogGender;
+    @BindView(R.id.et_dog_age)
+    EditText etDogAge;
     @BindView(R.id.et_dog_description)
     EditText etDogDescription;
+    @BindView(R.id.et_dog_size)
+    EditText etDogSize;
+    @BindView(R.id.et_dog_weight)
+    EditText etDogWeight;
+    @BindView(R.id.et_dog_location)
+    EditText etDogLocation;
+    @BindView(R.id.et_dog_attitude_people)
+    EditText etDogAttitudePeople;
+    @BindView(R.id.et_dog_attitude_dogs)
+    EditText etDogAttitudeDogs;
+    @BindView(R.id.et_dog_attitude_cats)
+    EditText getEtDogAttitudeCats;
+    @BindView(R.id.et_dog_keeper_name)
+    EditText etDogKeeperName;
+    @BindView(R.id.et_dog_keeper_phone)
+    EditText etDogKeeperPhone;
+    @BindView(R.id.et_dog_keeper_mail)
+    EditText etDogKeeperMail;
+    @BindView(R.id.et_dog_homeless_since)
+    EditText etDogHomelessSince;
 
     // endregion
 
@@ -78,10 +100,9 @@ public class FirebaseTesting extends AppCompatActivity implements FirebaseDataLi
     @OnClick(R.id.btn_add_new_dog)
     void addNewDogDog(View view) {
         String dogName = etDogName.getText().toString();
-        String dogCategory = etDogCateory.getText().toString();
         String dogDescription = etDogDescription.getText().toString();
 
-        firebaseHelper.writeNewDog(dogName, dogCategory, dogDescription);
+        firebaseHelper.writeNewDog(dogName, "category", dogDescription);
 
         Toast.makeText(getBaseContext(), "New dog added", Toast.LENGTH_SHORT).show();
     }

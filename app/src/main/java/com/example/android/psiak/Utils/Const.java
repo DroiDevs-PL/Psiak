a891298c8fc308f33bdfdac0db53ec1af146e949
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2017-11-18 11:05:41 +0100

    ADD Helper Class For Storing Constants

---
+++ app/src/main/java/com/example/android/psiak/Utils/Const.java
@@ -0,0 +1,29 @@
+package com.example.android.psiak.Utils;
+
+/**
+ * Created by grzegorz.kwasniewski on 18.11.2017.
+ */
+
+public class Const {
+
+    public static final String NAME = "name";
+    public static final String GENDER = "gender";
+    public static final String DESCRIPTION = "description";
+    public static final String SIZE = "size";
+    public static final String LOCATION = "location";
+    public static final String HOMELESS_SINCE = "homeless_since";
+    public static final String ATTITUDE_PEOPLE = "attitude_people";
+    public static final String ATTITUDE_DOGS = "attitude_dogs";
+    public static final String ATTITUDE_CATS = "attitude_cats";
+    public static final String KEEPER_NAME = "keeper_name";
+    public static final String KEEPER_MAIL = "keeper_mail";
+    public static final String KEEPER_PHONE = "keeper_phone";
+    public static final String VACCINATED = "vaccinated";
+    public static final String DEWORMED = "dewormed";
+    public static final String STERILIZED = "sterilized";
+    public static final String WEIGHT = "weight";
+    public static final String AGE = "age";
+
+    public static final String NO_DATA = "no data";
+
+}

346cbde96011b7ad7666305baffe353276f9162a
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2017-11-20 19:09:01 +0100

    Fix minor fixes

--- app/src/main/java/com/example/android/psiak/Utils/AutoValueGsonFactory.java
+++ app/src/main/java/com/example/android/psiak/Utils/AutoValueGsonFactory.java
@@ -7,6 +7,6 @@ public abstract class AutoValueGsonFactory implements TypeAdapterFactory {
     // Static factory method to access the package
     // private generated implementation
     public static TypeAdapterFactory create() {
-        return new AutoValueGson_AutoValueGsonFactory();
+        return null;
     }
 }

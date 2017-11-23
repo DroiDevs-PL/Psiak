c0c6b963319075a7d8ac82f7760a956e5569547f
Maciej Bialorucki <maciej.bialorucki@gmail.com>
2017-11-23 14:25:23 +0100

    Add Mockito library and example code

--- app/src/test/java/com/example/android/psiak/AddAnimal/AddAnimalPresenterTest.java
+++ app/src/test/java/com/example/android/psiak/AddAnimal/AddAnimalPresenterTest.java
@@ -2,59 +2,51 @@ package com.example.android.psiak.AddAnimal;
 
 import com.example.android.psiak.Repository.InMemoryRepository;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 /**
  * Created by Maciej Bialorucki on 20.11.17.
  */
 public class AddAnimalPresenterTest {
 
+    private static AddAnimalContract.View mockView;
+
+    @Before
+    public void init() {
+        mockView = mock(AddAnimalActivity.class);
+    }
+
     @Test
     public void should_be_one_dog_after_save() {
         InMemoryRepository repository = InMemoryRepository.getInstance();
         AddAnimalPresenter presenter = new AddAnimalPresenter(repository);
-        presenter.attach(new MockView());
+        presenter.attach(mockView);
         presenter.save("Burek", "10 lat");
         assertThat(repository.getAll().size(), is(1));
     }
 
     @Test
-    public void should_show_success_method_after_add(){
+    public void should_show_success_method_after_add() {
         InMemoryRepository repository = InMemoryRepository.getInstance();
         AddAnimalPresenter presenter = new AddAnimalPresenter(repository);
-        MockView view = new MockView();
-        presenter.attach(view);
+        presenter.attach(mockView);
         presenter.save("Burek", "10 lat");
-        assertThat(view.success,is(true));
+        verify(mockView).showSuccessMessage();
     }
 
     @Test
-    public void should_show_error_when_empty_dog_data(){
+    public void should_show_error_when_empty_dog_data() {
         InMemoryRepository repository = InMemoryRepository.getInstance();
         AddAnimalPresenter presenter = new AddAnimalPresenter(repository);
-        MockView view = new MockView();
-        presenter.attach(view);
-        presenter.save("","");
-        assertThat(view.error,is(true));
-
+        presenter.attach(mockView);
+        presenter.save("", "");
+        verify(mockView).showErrorMessage();
     }
 
-    private class MockView implements AddAnimalContract.View {
-
-        boolean success;
-        boolean error;
-
-        @Override
-        public void showSuccessMessage() {
-            success = true;
-        }
-
-        @Override
-        public void showErrorMessage() {
-            error = true;
-        }
-    }
 }

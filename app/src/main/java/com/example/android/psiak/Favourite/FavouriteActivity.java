be6f91c21d17d4e679bae32fab48e27f1ed44834
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2017-12-04 23:14:39 +0100

    ADD New Activity For Favourite Screen

---
+++ app/src/main/java/com/example/android/psiak/Favourite/FavouriteActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.psiak.Favourite;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.android.psiak.R;
+
+public class FavouriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourite);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}

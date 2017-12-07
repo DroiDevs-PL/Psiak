e767e17e54554d15feec69f1c80aff0f745e9ccd
Grzegorz Kwaśniewski <grzegrz.kwasniewski2@gmail.com>
2017-12-07 15:07:52 +0100

    ADD RecyclerView To Favourite Screen

--- app/src/main/java/com/example/android/psiak/FavouriteView/FavouriteActivity.java
+++ app/src/main/java/com/example/android/psiak/FavouriteView/FavouriteActivity.java
@@ -1,18 +1,56 @@
 package com.example.android.psiak.FavouriteView;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 
 import com.example.android.psiak.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FavouriteActivity extends AppCompatActivity {
+
+    // region Private Properties
+
+    @BindView(R.id.favourites_recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.favourites_root_layout)
+    ConstraintLayout rootLayout;
+
+    private DogAdapter dogAdapter;
+    private LinearLayoutManager layoutManager;
+
+    // endregion
+
+    // region Activity Lifecycle
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favourite);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        dogAdapter = new DogAdapter(this, layoutManager, rootLayout);
+
+        recyclerView.setAdapter(dogAdapter);
     }
+
+    // endregion
 }

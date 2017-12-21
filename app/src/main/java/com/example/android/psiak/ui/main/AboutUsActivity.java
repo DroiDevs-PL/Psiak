524a2f249a462ad707b7fa5de5d372bbc74decf0
kaska kubat <katarz.kubat@gmail.com>
2017-12-21 20:57:17 +0100

    move aboutUs to main

--- app/src/main/java/com/example/android/psiak/ui/main/AboutUsActivity.java
+++ app/src/main/java/com/example/android/psiak/ui/main/AboutUsActivity.java
@@ -1,7 +1,9 @@
-package com.example.android.psiak;
+package com.example.android.psiak.ui.main;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.example.android.psiak.R;
 
 public class AboutUsActivity extends AppCompatActivity {
 

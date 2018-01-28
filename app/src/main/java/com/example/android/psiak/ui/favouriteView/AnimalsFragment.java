9a56f2e15aa4e08a50e8ac4592ab5f2963c65206
Grzegorz Kwaśniewski <contact@grzegorzkwasniewski.com>
2018-01-28 20:06:27 +0100

    Grzegorz favourite view mvp (#27)
    * UPDATE Tabs In Pager Adapter
    
    * ADD FavouriteView Presenter
    
    * ADD Favourite Contract
    
    * UPDATE Document Code
    
    * UPDATE Implement MVP For Favourite View
    
    * UPDATE Implement MVP For Favourite View
    
    * UPDATE Tabs Count

--- app/src/main/java/com/example/android/psiak/ui/favouriteView/AnimalsFragment.java
+++ app/src/main/java/com/example/android/psiak/ui/favouriteView/AnimalsFragment.java
@@ -1,9 +1,9 @@
 package com.example.android.psiak.ui.favouriteView;
 
-import android.support.design.widget.CoordinatorLayout;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,16 +11,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.android.psiak.R;
 import com.example.android.psiak.data.local.DogsLocalRepository;
 import com.example.android.psiak.model.DogFirebase;
-import com.example.android.psiak.R;
-import com.example.android.psiak.data.network.Repository;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.realm.RealmResults;
 
-public class AllAnimalsFragment extends Fragment {
+public class AnimalsFragment
+        extends Fragment
+        implements FavouriteViewContract.View  {
 
     // region Properties
 
@@ -29,11 +30,14 @@ public class AllAnimalsFragment extends Fragment {
     @BindView(R.id.favourites_root_layout)
     CoordinatorLayout rootLayout;
 
+    /**
+     * Presenter for this view
+     */
+
+    private FavouriteViewContract.Presenter<FavouriteViewContract.View> favouritePresenter;
+
     private DogAdapter dogAdapter;
     private LinearLayoutManager layoutManager;
-
-    private Repository.LocalRepository localRepository;
-
 
     // endregion
 
@@ -44,9 +48,10 @@ public class AllAnimalsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_tab_all_animals, container, false);
         ButterKnife.bind(this, view);
 
-        // TODO: Use Presenter
-        localRepository = new DogsLocalRepository(getContext());
-        RealmResults<DogFirebase> dogsCollection = localRepository.getAll();
+        // TODO: Dependency Injection
+        favouritePresenter = new FavouritePresenter(new DogsLocalRepository(getContext()));
+        favouritePresenter.attachView(this);
+        RealmResults<DogFirebase> dogsCollection = favouritePresenter.getAllDogsFromLocalRepository();
 
         recyclerView.setHasFixedSize(true);
 
@@ -65,4 +70,17 @@ public class AllAnimalsFragment extends Fragment {
 
     // endregion
 
+    // region Public Methods
+
+    @Override
+    public void showMessage(int messageId) {
+
+    }
+
+    @Override
+    public void showErrorMessage(String errorMessage) {
+
+    }
+
+    // endregion
 }

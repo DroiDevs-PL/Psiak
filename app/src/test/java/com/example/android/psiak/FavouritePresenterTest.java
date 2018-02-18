8f727869e4f3f7fbba2ad803992dee1c1c3cd90a
Grzegorz Kwasniewski <contact@grzegorzkwasniewski.com>
2018-02-18 12:24:30 +0100

    TEST Show Favourite Animals With Proper Fetch From DB

--- app/src/test/java/com/example/android/psiak/FavouritePresenterTest.java
+++ app/src/test/java/com/example/android/psiak/FavouritePresenterTest.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import com.example.android.psiak.data.local.DogsLocalRepository;
 import com.example.android.psiak.data.network.Repository;
+import com.example.android.psiak.model.Dog;
 import com.example.android.psiak.model.DogFirebase;
 import com.example.android.psiak.ui.favouriteView.AnimalsFragment;
 import com.example.android.psiak.ui.favouriteView.FavouritePresenter;
@@ -14,7 +15,11 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import io.realm.RealmResults;
+
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class FavouritePresenterTest {
 
@@ -32,8 +37,24 @@ public class FavouritePresenterTest {
     }
 
     @Test
-    public void show_animals_after_database_fetch() {
+    public void show_no_animals_after_database_fetch() {
         favouritePresenter.getAllAnimalsFromLocalRepository();
         verify(view).showErrorMessage("Error");
+    }
+
+    @Test
+    public void show_animals_after_database_fetch() {
+
+        RealmResults<DogFirebase> dogsFirebase = mockRealmResults();
+        when(localRepository.getAll()).thenReturn(dogsFirebase);
+
+        favouritePresenter.getAllAnimalsFromLocalRepository();
+
+        verify(view).showFavouriteAnimals(localRepository.getAll());
+    }
+
+    @SuppressWarnings("unchecked")
+    private RealmResults<DogFirebase> mockRealmResults() {
+        return mock(RealmResults.class);
     }
 }
